2c488fbdf0a560fb3fd63cf2403c059e0f0cd78d
jackson.dalexander <jackson.dalexander@MU-C1-10.mullen.pvt.k12.co.us>
2021-12-15 13:21:23 -0700

    changes

--- Inheritance Quizzer/src/InheritanceRunner.java
+++ Inheritance Quizzer/src/InheritanceRunner.java
@@ -4,18 +4,346 @@ public class InheritanceRunner {
 	static Scanner UserIntInput = new Scanner(System.in);
 	public static void main(String[] args) {
 		int counter = 0;
-		System.out.println("This is a prgram which will test you on your inheritance skills!/n");
-		
+		System.out.println("This is a program which will test you on your inheritance skills!");
+		System.out.println();
 		System.out.println("If this code runs: ");
-		System.out.println("Sailboat x = new Sailboat;");
+		System.out.println("Sailboat x = new Sailboat();");
 		System.out.println("x.rigBoat();");
 		System.out.println("What will be printed out?");
+		System.out.println();
 		
 		System.out.println("(1) Set up the boat!");
 		System.out.println("(2) Point the boat downwind!");
 		System.out.println("(3) Flip the boat over and get in the water.");
 		System.out.println("(4) Point the boat downwind and put the pole out to let the jib sail out!");
 		System.out.println("(5) error");
+		
+		int userChoice = UserIntInput.nextInt();
+		
+		if(userChoice == 1) {
+			System.out.println("That was the right answer!  ====");
+			counter++;
+			Sailboat x = new Sailboat();
+			x.rigBoat();
+			
+		}
+		else {
+			System.out.println("WRONG");
+			Sailboat x = new Sailboat();
+			System.out.println("It should have printed out:");
+			x.rigBoat();
+			
+		}
+		
+		System.out.println("If this code runs: ");
+		System.out.println("Sailboat x = new Sailboat();");
+		System.out.println("x.sailDownwind();");
+		System.out.println("What will be printed out?");
+		System.out.println();
+		
+		System.out.println("(1) Set up the boat!");
+		System.out.println("(2) Point the boat downwind!");
+		System.out.println("(3) Flip the boat over and get in the water.");
+		System.out.println("(4) Point the boat downwind and put the pole out to let the jib sail out!");
+		System.out.println("(5) error");
+		
+		userChoice = UserIntInput.nextInt();
+		
+		if(userChoice == 2) {
+			System.out.println("That was the right answer!  ====");
+			counter++;
+			Sailboat x = new Sailboat();
+			x.sailDownwind();
+			
+		}
+		else {
+			System.out.println("WRONG");
+			Sailboat x = new Sailboat();
+			System.out.println("It should have printed out:");
+			x.sailDownwind();
+			
+		}
+		System.out.println("If this code runs: ");
+		System.out.println("Sailboat x = new Sailboat();");
+		System.out.println("x.capsize();");
+		System.out.println("What will be printed out?");
+		System.out.println();
+		
+		System.out.println("(1) Set up the boat!");
+		System.out.println("(2) Point the boat downwind!");
+		System.out.println("(3) Flip the boat over and get in the water.");
+		System.out.println("(4) Point the boat downwind and put the pole out to let the jib sail out!");
+		System.out.println("(5) error");
+		
+		userChoice = UserIntInput.nextInt();
+		
+		if(userChoice == 5) {
+			System.out.println("That was the right answer!  ====");
+			counter++;
+			Sailboat x = new Sailboat();
+			x.rigBoat();
+			
+		}
+		else {
+			System.out.println("WRONG");
+			Sailboat x = new Sailboat();
+			System.out.println("It should have printed out:");
+			x.rigBoat();
+			
+		}
+		System.out.println("If this code runs: ");
+		System.out.println("Sailboat x = new Sailboat();");
+		System.out.println("x.rigBoat();");
+		System.out.println("What will be printed out?");
+		System.out.println();
+		
+		System.out.println("(1) Set up the boat!");
+		System.out.println("(2) Point the boat downwind!");
+		System.out.println("(3) Flip the boat over and get in the water.");
+		System.out.println("(4) Point the boat downwind and put the pole out to let the jib sail out!");
+		System.out.println("(5) error");
+		
+		userChoice = UserIntInput.nextInt();
+		
+		if(userChoice == 1) {
+			System.out.println("That was the right answer!  ====");
+			counter++;
+			Sailboat x = new Sailboat();
+			x.rigBoat();
+			
+		}
+		else {
+			System.out.println("WRONG");
+			Sailboat x = new Sailboat();
+			System.out.println("It should have printed out:");
+			x.rigBoat();
+			
+		}
+		System.out.println("If this code runs: ");
+		System.out.println("Sailboat x = new Sailboat();");
+		System.out.println("x.rigBoat();");
+		System.out.println("What will be printed out?");
+		System.out.println();
+		
+		System.out.println("(1) Set up the boat!");
+		System.out.println("(2) Point the boat downwind!");
+		System.out.println("(3) Flip the boat over and get in the water.");
+		System.out.println("(4) Point the boat downwind and put the pole out to let the jib sail out!");
+		System.out.println("(5) error");
+		
+		userChoice = UserIntInput.nextInt();
+		
+		if(userChoice == 1) {
+			System.out.println("That was the right answer!  ====");
+			counter++;
+			Sailboat x = new Sailboat();
+			x.rigBoat();
+			
+		}
+		else {
+			System.out.println("WRONG");
+			Sailboat x = new Sailboat();
+			System.out.println("It should have printed out:");
+			x.rigBoat();
+			
+		}
+		System.out.println("If this code runs: ");
+		System.out.println("Sailboat x = new Sailboat();");
+		System.out.println("x.rigBoat();");
+		System.out.println("What will be printed out?");
+		System.out.println();
+		
+		System.out.println("(1) Set up the boat!");
+		System.out.println("(2) Point the boat downwind!");
+		System.out.println("(3) Flip the boat over and get in the water.");
+		System.out.println("(4) Point the boat downwind and put the pole out to let the jib sail out!");
+		System.out.println("(5) error");
+		
+		userChoice = UserIntInput.nextInt();
+		
+		if(userChoice == 1) {
+			System.out.println("That was the right answer!  ====");
+			counter++;
+			Sailboat x = new Sailboat();
+			x.rigBoat();
+			
+		}
+		else {
+			System.out.println("WRONG");
+			Sailboat x = new Sailboat();
+			System.out.println("It should have printed out:");
+			x.rigBoat();
+			
+		}
+		System.out.println("If this code runs: ");
+		System.out.println("Sailboat x = new Sailboat();");
+		System.out.println("x.rigBoat();");
+		System.out.println("What will be printed out?");
+		System.out.println();
+		
+		System.out.println("(1) Set up the boat!");
+		System.out.println("(2) Point the boat downwind!");
+		System.out.println("(3) Flip the boat over and get in the water.");
+		System.out.println("(4) Point the boat downwind and put the pole out to let the jib sail out!");
+		System.out.println("(5) error");
+		
+		userChoice = UserIntInput.nextInt();
+		
+		if(userChoice == 1) {
+			System.out.println("That was the right answer!  ====");
+			counter++;
+			Sailboat x = new Sailboat();
+			x.rigBoat();
+			
+		}
+		else {
+			System.out.println("WRONG");
+			Sailboat x = new Sailboat();
+			System.out.println("It should have printed out:");
+			x.rigBoat();
+			
+		}
+		System.out.println("If this code runs: ");
+		System.out.println("Sailboat x = new Sailboat();");
+		System.out.println("x.rigBoat();");
+		System.out.println("What will be printed out?");
+		System.out.println();
+		
+		System.out.println("(1) Set up the boat!");
+		System.out.println("(2) Point the boat downwind!");
+		System.out.println("(3) Flip the boat over and get in the water.");
+		System.out.println("(4) Point the boat downwind and put the pole out to let the jib sail out!");
+		System.out.println("(5) error");
+		
+		userChoice = UserIntInput.nextInt();
+		
+		if(userChoice == 1) {
+			System.out.println("That was the right answer!  ====");
+			counter++;
+			Sailboat x = new Sailboat();
+			x.rigBoat();
+			
+		}
+		else {
+			System.out.println("WRONG");
+			Sailboat x = new Sailboat();
+			System.out.println("It should have printed out:");
+			x.rigBoat();
+			
+		}
+		System.out.println("If this code runs: ");
+		System.out.println("Sailboat x = new Sailboat();");
+		System.out.println("x.rigBoat();");
+		System.out.println("What will be printed out?");
+		System.out.println();
+		
+		System.out.println("(1) Set up the boat!");
+		System.out.println("(2) Point the boat downwind!");
+		System.out.println("(3) Flip the boat over and get in the water.");
+		System.out.println("(4) Point the boat downwind and put the pole out to let the jib sail out!");
+		System.out.println("(5) error");
+		
+		userChoice = UserIntInput.nextInt();
+		
+		if(userChoice == 1) {
+			System.out.println("That was the right answer!  ====");
+			counter++;
+			Sailboat x = new Sailboat();
+			x.rigBoat();
+			
+		}
+		else {
+			System.out.println("WRONG");
+			Sailboat x = new Sailboat();
+			System.out.println("It should have printed out:");
+			x.rigBoat();
+			
+		}
+		System.out.println("If this code runs: ");
+		System.out.println("Sailboat x = new Sailboat();");
+		System.out.println("x.rigBoat();");
+		System.out.println("What will be printed out?");
+		System.out.println();
+		
+		System.out.println("(1) Set up the boat!");
+		System.out.println("(2) Point the boat downwind!");
+		System.out.println("(3) Flip the boat over and get in the water.");
+		System.out.println("(4) Point the boat downwind and put the pole out to let the jib sail out!");
+		System.out.println("(5) error");
+		
+		userChoice = UserIntInput.nextInt();
+		
+		if(userChoice == 1) {
+			System.out.println("That was the right answer!  ====");
+			counter++;
+			Sailboat x = new Sailboat();
+			x.rigBoat();
+			
+		}
+		else {
+			System.out.println("WRONG");
+			Sailboat x = new Sailboat();
+			System.out.println("It should have printed out:");
+			x.rigBoat();
+			
+		}
+		System.out.println("If this code runs: ");
+		System.out.println("Sailboat x = new Sailboat();");
+		System.out.println("x.rigBoat();");
+		System.out.println("What will be printed out?");
+		System.out.println();
+		
+		System.out.println("(1) Set up the boat!");
+		System.out.println("(2) Point the boat downwind!");
+		System.out.println("(3) Flip the boat over and get in the water.");
+		System.out.println("(4) Point the boat downwind and put the pole out to let the jib sail out!");
+		System.out.println("(5) error");
+		
+		userChoice = UserIntInput.nextInt();
+		
+		if(userChoice == 1) {
+			System.out.println("That was the right answer!  ====");
+			counter++;
+			Sailboat x = new Sailboat();
+			x.rigBoat();
+			
+		}
+		else {
+			System.out.println("WRONG");
+			Sailboat x = new Sailboat();
+			System.out.println("It should have printed out:");
+			x.rigBoat();
+			
+		}
+		System.out.println("If this code runs: ");
+		System.out.println("Sailboat x = new Sailboat();");
+		System.out.println("x.rigBoat();");
+		System.out.println("What will be printed out?");
+		System.out.println();
+		
+		System.out.println("(1) Set up the boat!");
+		System.out.println("(2) Point the boat downwind!");
+		System.out.println("(3) Flip the boat over and get in the water.");
+		System.out.println("(4) Point the boat downwind and put the pole out to let the jib sail out!");
+		System.out.println("(5) error");
+		
+		userChoice = UserIntInput.nextInt();
+		
+		if(userChoice == 1) {
+			System.out.println("That was the right answer!  ====");
+			counter++;
+			Sailboat x = new Sailboat();
+			x.rigBoat();
+			
+		}
+		else {
+			System.out.println("WRONG");
+			Sailboat x = new Sailboat();
+			System.out.println("It should have printed out:");
+			x.rigBoat();
+			
+		}
+		
 		
 
 	}
